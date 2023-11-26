aaad1126f576fc76d07b91cb97bbf1835c18e2da
fsaeo <152092729+fsaeo@users.noreply.github.com>
2023-11-26 21:27:59 +0300

    Added renderer
    working with flask

---
+++ floew-renderer.py
@@ -0,0 +1,26 @@
+from flask import *
+from FloewMarker import floewParser
+
+app = Flask(__name__)
+@app.route("/")
+def homepage(): 
+    template =floewParser.fparse("""
+    @~~ comment
+    text
+                                      ;;text;;
+                                      //text// 
+                                      __text__ 
+                                      ``text`` 
+                                      ~~text
+                                    text~~ 
+                                 #heading#
+                                        """)
+    context = {}
+    return template
+
+app.run(debug=True)
+
+# print(floewParser.fparse("""@~~ comment <br>
+#      newline here //italic//
+#     ;;bold;; """))
+
